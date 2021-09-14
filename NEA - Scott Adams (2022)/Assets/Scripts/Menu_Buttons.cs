ac1318b75d88e975fb23d266f3df67fc4d1fcfd1
adams-s01 <85705721+adams-s01@users.noreply.github.com>
2021-09-14 11:00:31 +0100

    Sprint 1

---
+++ NEA - Scott Adams (2022)/Assets/Scripts/Menu_Buttons.cs
@@ -0,0 +1,33 @@
+﻿/*
+	 * Created: Sprint 1
+	 * Last Edited: Sprint 1
+	 * Purpose: Start menu buttons
+	 */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Buttons : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	//Activates when start button clicked
+	void Startbutton()
+	{
+		Debug.Log("Start");
+	}
+	//Activates when tutorial button clicked
+	void Tutorialbutton()
+	{
+		Debug.Log("Tutorial");
+	}
+}
