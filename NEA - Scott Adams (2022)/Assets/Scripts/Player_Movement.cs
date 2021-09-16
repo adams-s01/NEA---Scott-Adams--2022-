e2ae04cf6664a8d23328f196b55c17a73023fcf5
adams-s01 <85705721+adams-s01@users.noreply.github.com>
2021-09-16 21:29:26 +0100

    Sprint 1.3

--- NEA - Scott Adams (2022)/Assets/Scripts/Player_Movement.cs
+++ NEA - Scott Adams (2022)/Assets/Scripts/Player_Movement.cs
@@ -12,36 +12,50 @@ public class Player_Movement : MonoBehaviour {
 	Rigidbody2D rb;
 	GameObject leg1;
 	GameObject leg;
+	int speed;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
 		leg1 = GameObject.FindGameObjectWithTag ("leg1");
 		leg = GameObject.FindGameObjectWithTag ("leg");
+		speed = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//Moves right
 		if (Input.GetKey(KeyCode.RightArrow))
 		{
-			transform.Translate (Vector3.right * Time.deltaTime*2);
+			transform.Translate (Vector3.right * speed * Time.deltaTime * 3);
 			if(leg1.transform.eulerAngles.y==0)
 				{
-				leg1.transform.Rotate(0,180,0);
-				leg.transform.Rotate(0,180,0);
+				Debug.Log (leg1.transform.eulerAngles.y);
+				leg1.transform.localEulerAngles = new Vector3 (0, 180, 0);
+				leg.transform.localEulerAngles = new Vector3 (0, 180, 0);
 				}
 		}
+		//Moves left
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			transform.Translate (Vector3.left * Time.deltaTime * 2);
+			transform.Translate (Vector3.left * speed * Time.deltaTime * 3);
 			if(leg1.transform.eulerAngles.y==180)
 				{
-				leg1.transform.Rotate(0,180,0);
-				leg.transform.Rotate(0,180,0);
+				Debug.Log (leg1.transform.eulerAngles.y);
+				leg1.transform.localEulerAngles = new Vector3 (0, 0, 0);
+				leg.transform.localEulerAngles = new Vector3 (0, 0, 0);
 				}
 		}
+		//Jumps
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			transform.Translate (Vector3.up);
+			rb.AddForce (new Vector2 (0, 2) * 2, ForceMode2D.Impulse);
+
 		}
-		
+		//Sprint
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			speed = 10;
+		}
+		if (Input.GetKey (KeyCode.LeftShift) == false) {
+			speed = 1;
+		}
 	}
 }
