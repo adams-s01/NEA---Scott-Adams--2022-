1ea929b37edc2bf3b8783f3599a970e689776edd
adams-s01 <85705721+adams-s01@users.noreply.github.com>
2022-01-07 15:50:43 +0000

    Sprint 12.3

--- NEA - Scott Adams (2022)/Assets/Scripts/Score_Table.cs
+++ NEA - Scott Adams (2022)/Assets/Scripts/Score_Table.cs
@@ -22,14 +22,28 @@ public class Score_Table : MonoBehaviour {
 	public TextMeshProUGUI second;
 	public TextMeshProUGUI third;
 	public TextMeshProUGUI fourth;
-	public TextMeshProUGUI five;
+	public TextMeshProUGUI fifth;
+	public String[] array;
+	public int count;
+	bool twice;
+	int i;
+	string read;
+	public GameObject scoreboard;
+	public GameObject menu;
+	TMP_InputField input;
+	string name;
 
 	// Use this for initialization
 	void Start () {
 		if (SceneManager.GetActiveScene ().buildIndex != 0) {
 			other2 = other.GetComponent<Escape_Door> ();
+			input = GameObject.FindGameObjectWithTag ("Name").GetComponent<TMP_InputField> ();
+
 		}
 		filepath = getpath ();
+		if (SceneManager.GetActiveScene ().buildIndex == 0) {
+			scoreboard.SetActive (false);
+		}
 	}
 	
 	// Update is called once per frame
@@ -40,19 +54,49 @@ public class Score_Table : MonoBehaviour {
 				//Writes name and time to the CSV file
 				once = true;
 				StreamWriter writer = new StreamWriter (filepath, true);
-				writer.WriteLine ("Name," + Time.fixedTime.ToString ("0.00"));
+				name = input.text;
+				writer.WriteLine (Time.fixedTime.ToString ("00.00") + "," + name);
 				writer.Close ();
 				Debug.Log ("2345");
 			}
 		}
+		//For the score page showing the top five
 		if (SceneManager.GetActiveScene ().buildIndex == 0) {
 			StreamReader reader = new StreamReader (filepath);
-
+			//Counts how many items in file
+			if (twice == false) {
+				twice = true;
+				do {
+					count++;
+				} while(reader.ReadLine () != null);
+			}
+			array = new String[count-1];
+			i = 0;
+			//Adds items into an array
+			while (!reader.EndOfStream) {
+				//Debug.Log (reader.ReadLine ());
+				array [i] = reader.ReadLine ();
+				i++;
+			}
+			reader.Close ();
+			//Organises array in order of shortest time to longest
+			Array.Sort (array);
+			//Shows the top five times
+			first.text = "1: " + array [0];
+			second.text = "2: " + array [1];
+			third.text = "3: " + array [2];
+			fourth.text = "4: " + array [3];
+			fifth.text = "5: " + array [4];
 		}
 	}
 	//Finds location of the file
 	private string getpath()
 	{
 		return Application.dataPath + "ScoreTable.csv";
+	}
+	public void ExitButton()
+	{
+		scoreboard.SetActive (false);
+		menu.SetActive (true);
 	}
 }
